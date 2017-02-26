4d8c9d3bd854e429237984bf7394955f1ff9027a
Duodecimus <dmhamilt@ualberta.ca>
2017-02-26 08:08:19 -0700

    Changed instructions to include Cybera Volumes

--- Mysql/sentiment_dict_3mil.sql
+++ Mysql/sentiment_dict_3mil.sql
@@ -1,3 +1,15 @@
+#attach volume with cybera https://docs.google.com/document/d/12_iH7oFfP2MTBi7wCR92PiIalhsB8i2bcz2G89wUsmk/edit#
+#Format the volume:
+#	$ sudo mkfs.ext4 /dev/sdc
+#Note: attached volumes will typically be assigned device names in sequential order (i.e. /dev/sdc, /dev/sdd, /dev/sde, etc.) List all disks from within the instance with 
+#$ sudo fdisk -l. /dev/sda and /dev/sdb are the system volumes that make up the instance.
+#Create a mount point for the volume:
+#	$ sudo mkdir /mnt/<mount_point_name>
+#Mount the volume device to the mount point:
+#$ sudo mount /dev/sdc /mnt/<mount_point_name>
+#Permissions may need to be changed on the new volume, as they are initially set to root:
+#	$ sudo chown ubuntu:ubuntu /mnt/<mount_point_name>
+
 #install mysql:
 #sudo apt-get update
 #sudo apt-get install mysql-server
@@ -6,21 +18,27 @@
 #Download the csv files at: 
 # http://www.ualberta.ca/~hollis/files/sentiment_dict_3mil.tgz
 # http://www.ualberta.ca/~hollis/files/articles-can.tgz
+#wget is linux's command to download, but it wouldn't work for me
 
 #on your local computer, use this command to send the file. This is done on a windows computer with Putty installed
 #ex: pscp -l username -i "C:\Path\to\key.ppk" "c:\file.foo" 199.116.235.53:file.foo
-# pscp -l ubuntu -i "C:\Users\Owner\Desktop\401\putty words over time key.ppk" "C:\Users\Owner\Desktop\401\sentiment_dict_3mil.tgz" 199.116.235.53:/home/ubuntu/files/sentiment_dict_3mil.tgz
-# pscp -l ubuntu -i "C:\Users\Owner\Desktop\401\putty words over time key.ppk" "C:\Users\Owner\Desktop\401\articles-can.tgz" 199.116.235.53:/home/ubuntu/files/articles-can.tgz
+# pscp -l ubuntu -i "C:\Users\Owner\Desktop\401\putty words over time key.ppk" "C:\Users\Owner\Desktop\401\sentiment_dict_3mil.tgz" 199.116.235.53:/mnt/D/sentiment_dict_3mil.tgz
+# pscp -l ubuntu -i "C:\Users\Owner\Desktop\401\putty words over time key.ppk" "C:\Users\Owner\Desktop\401\articles-can.tgz" 199.116.235.53:/mnt/D/articles-can.tgz
 
-#following stuff is executed on the linuz machine
+#Move mysql onto the volume  http://stackoverflow.com/questions/1795176/how-to-change-mysql-data-directory
+sudo /etc/init.d/mysql stop
+sudo cp -R -p /var/lib/mysql /mnt/D
+sudo vim /etc/mysql/mysql.conf.d/mysqld.cnf #Look for the entry for datadir, and change the path (which should be /var/lib/mysql) to the new data directory.
+sudo vim /etc/apparmor.d/tunables/alias  # Add "alias /var/lib/mysql/ -> /newpath/,"
+sudo /etc/init.d/apparmor reload
+sudo /etc/init.d/mysql restart
 
 #Unpack the tgz with
 # tar zxvf sentiment_dict_3mil.tgz
 # tar zxvf articles-can.tgz
 
-#start mysql
+#start mysql with
 #mysql -u root -p
-
 
 
 
@@ -35,39 +53,34 @@ DROP TABLE IF EXISTS sentiment_dict_3mil;
 CREATE TABLE sentiment_dict_3mil
 (
  Word varchar(255) NOT NULL,
- Valence decimal(12,11) NOT NULL,
- Arousal decimal(12,11) NOT NULL,
- Dominance decimal(12,11) NOT NULL,
- Concreteness decimal(12,11) NOT NULL,
- AoA decimal(13,11) NOT NULL,
+ Valence decimal(15,13) NOT NULL,
+ Arousal decimal(15,13) NOT NULL,
+ Dominance decimal(15,13) NOT NULL,
+ Concreteness decimal(15,13) NOT NULL,
+ AoA decimal(15,13) NOT NULL,
 
  PRIMARY KEY (Word)
 );
 
-#This command should load the file off of the system you're sshing from, but it doesn't for some reason. interent says permission issues?
-#load data local infile "C:\\sentiment_dict_3mil.csv" into table Client_Generated_Data.sentiment_dict_3mil FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (Word, Valence, Arousal, Dominance, Concreteness, AoA);
+load data local infile "/mnt/D/sentiment_dict_3mil.csv" into table Client_Generated_Data.sentiment_dict_3mil FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (Word, Valence, Arousal, Dominance, Concreteness, AoA);
 
-load data infile "/home/ubuntu/files/sentiment_dict_3mil.csv" into table Client_Generated_Data.sentiment_dict_3mil FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (Word, Valence, Arousal, Dominance, Concreteness, AoA);
+drop table if exists articles_can;
 
-drop table if exists articles-can;
-
-create table articles-can
+create table articles_can
 (
  articleID int unsigned NOT NULL,
  language varchar(255) NOT NULL,
  province char(2) NOT NULL,
  city varchar(255) NOT NULL,
  country char(3) NOT NULL,
- publicationdate date NOT NULL,
+ publication_date date NOT NULL,
  wordcount int unsigned NOT NULL,
- parsedarticle text NOT NULL,
+ parsed_article text NOT NULL,
 
  PRIMARY KEY (articleID)
-)
+);
 
-#load data local infile "C:\\articles-can.csv" into table Client_Generated_Data.articles-can FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (articleID, language, province, city, country, publicationdate, wordcount, parsedarticle);
-
-load data infile "/home/ubuntu/files/articles-can.csv" into table Client_Generated_Data.sentiment_dict_3mil FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (Word, Valence, Arousal, Dominance, Concreteness, AoA);
+load data local infile "/mnt/D/articles-can.csv" into table Client_Generated_Data.articles_can FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (articleID, language, province, city, country, publication_date, wordcount, parsed_article);
 
 
 USE Precalculated_Data;
@@ -76,52 +89,51 @@ DROP TABLE IF EXISTS Document_Data;
 
 create table Document_Data
 (
- article_ID int unsigned NOT NULL,
+ articleID int unsigned NOT NULL,
  language varchar(255) NOT NULL,
  province char(2) NOT NULL,
  city varchar(255) NOT NULL,
  country char(3) NOT NULL,
  publication_date date NOT NULL,
  word_count int unsigned NOT NULL,
- Length int unsigned NOT NULL,
- Word_One varchar(255) NOT NULL,
- Word_Two varchar(255) NOT NULL,
- Word_Three varchar(255) NOT NULL,
- Word_Four varchar(255) NOT NULL,
- Word_Five varchar(255) NOT NULL,
- Average_Arousal_Doc decimal(12,11) NOT NULL,
- Average_Valence_Doc decimal(12,11) NOT NULL,
- Average_Arousal_Words decimal(12,11) NOT NULL,
- Average_Valence_Words decimal(12,11) NOT NULL,
+ word_one varchar(255) NOT NULL,
+ word_two varchar(255) NOT NULL,
+ word_three varchar(255) NOT NULL,
+ word_four varchar(255) NOT NULL,
+ word_five varchar(255) NOT NULL,
+ average_arousal_doc decimal(13,12) NOT NULL,
+ average_valence_doc decimal(13,12) NOT NULL,
+ average_arousal_words decimal(13,12) NOT NULL,
+ average_valence_words decimal(13,12) NOT NULL,
 
- PRIMARY KEY (article_ID)
-)
+ PRIMARY KEY (articleID)
+);
 
 DROP TABLE IF EXISTS Word_Data;
 
 CREATE TABLE Word_Data
 (
- Word varchar(255) NOT NULL,
- Article_ID int unsigned NOT NULL,
- Language varchar(255) NOT NULL,
+ word varchar(255) NOT NULL,
+ articleID int unsigned NOT NULL,
+ language varchar(255) NOT NULL,
  Province char(2) NOT NULL,
- City varchar(255) NOT NULL,
- Country char(3) NOT NULL,
- Publication_Date date NOT NULL,
- Word_Count int unsigned NOT NULL,
- Term_Frequency decimal(13,11) NOT NULL,
+ city varchar(255) NOT NULL,
+ country char(3) NOT NULL,
+ publication_Date date NOT NULL,
+ word_count int unsigned NOT NULL,
+ term_frequency decimal(14,12) NOT NULL,
 
- PRIMARY KEY (Word, Article_ID)
+ PRIMARY KEY (Word, articleID)
 );
 
 DROP TABLE IF EXISTS Corpus_Data;
 
 CREATE TABLE Corpus_Data
 (
- corpus_ID int unsigned NOT NULL,
- Inverse_document_Frequency decimal(13,11) NOT NULL,
- Start_Date date NOT NULL,
- End_Date date NOT NULL,
+ corpusID int unsigned NOT NULL,
+ inverse_document_Frequency decimal(14,12) NOT NULL,
+ start_date date NOT NULL,
+ end_date date NOT NULL,
 
- PRIMARY KEY (Word, Corpus_ID)
+ PRIMARY KEY (CorpusID)
 );
