8acec53a66784d5c12d191f08738eedc3f96e063
tommy3ua <tommy3@ualberta.ca>
2017-03-06 13:29:20 -0700

    cosine distance

--- mysite/words/dataanalyzer.py
+++ mysite/words/dataanalyzer.py
@@ -12,7 +12,12 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 #def averageArousalTopFive(chunk):
     
 def averageTfidfOfWord(chunk, word):
+    return
     
-def cosDistanceOfPair(chunk, word1, word2):
+def cosDistanceOfPair(chunk, word1, word2, cbow=True):
+    model = gensim.models.Word2Vec(chunk, min_count=1)
+    return model.similarity(word1, word2)
     
-def nClosestNeighborsOfWord(chunk, word, N):
+def nClosestNeighborsOfWord(chunk, word, N, cbow=True):
+    model = gensim.models.Word2Vec(chunk, min_count=1)
+    return model.most_similar(positive=[word], topn=N)
--- mysite/words/dataretrieval.py
+++ mysite/words/dataretrieval.py
@@ -4,8 +4,11 @@ import sys
 sys.path.append("C:/Users/L/Documents/School/WordsOverTime/mysite")
 import django
 django.setup()
+from models import Document
 
 # return a list of documents matching the query
 # query terms can be id, language, province, city, country, and date
 # for now just query by date range
 def getDocuments(startDate, endDate):
+    querySet = Document.objects.filter(publicationDate__lte='').filter(publicationDate__gte='')
+    return list(querySet)
