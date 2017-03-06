ca714c880e1548491e95fd69e93d92300e42c813
tommy3ua <tommy3@ualberta.ca>
2017-03-06 15:13:26 -0700

    cos distance

--- mysite/words/dataretrieval.py
+++ mysite/words/dataretrieval.py
@@ -4,11 +4,17 @@ import sys
 sys.path.append("C:/Users/L/Documents/School/WordsOverTime/mysite")
 import django
 django.setup()
-from models import Document
+from models import Document, Word, WordInDocument
 
-# return a list of documents matching the query
 # query terms can be id, language, province, city, country, and date
 # for now just query by date range
 def getDocuments(startDate, endDate):
-    querySet = Document.objects.filter(publicationDate__lte='').filter(publicationDate__gte='')
-    return list(querySet)
+    docs = Document.objects.filter(publicationDate__lte='').filter(publicationDate__gte='')
+    words = []
+    for doc in docs:
+        words.append(getWordsInDocument(doc))
+    return (list(docs), words)
+
+def getWordsInDocument(doc):
+    words = WordInDocument.objects.filter(document__article_id=doc.article_id)
+    return list(words)
