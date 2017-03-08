0f05a935f30f81985d1993a136e20c961b20ab2a
Main User Account <resler@ualberta.ca>
2017-03-08 11:06:05 -0700

    fixed some bugs

--- mysite/words/forms.py
+++ mysite/words/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class MainForm(forms.Form):
     keywords = forms.CharField(label= 'keywords', max_length = 100)
-    startDate = forms.CharField(label = 'startDate', max_length = 100)
-    endDate = forms.CharField(label = 'endDate', max_length = 100)
+    startDate = forms.DateField(label = 'startDate')
+    endDate = forms.DateField(label = 'endDate')
