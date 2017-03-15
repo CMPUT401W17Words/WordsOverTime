533939ea98ffd64b288a028aa3c0c0c76fb18ea8
tiegan <tiegan@ualberta.ca>
2017-03-15 00:49:43 -0600

    Django queries

--- mysite/words/models.py
+++ mysite/words/models.py
@@ -2,11 +2,55 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
-@python_2_unicode_compatible
-class Question(models.Model):
-  question_text = models.CharField(max_length=200)
-  pub_date = models.DateTimeField('date published')
-  def __str__(self):
-    return self.question_text
-  def was_published_recently(self):
-    return True
+
+class SentimentDictionary(models.Model):
+    word = models.CharField(max_length=255, primary_key=True)
+    valence = models.DecimalField(max_digits=15, decimal_places=13)
+    arousal = models.DecimalField(max_digits=15, decimal_places=13)
+    dominance = models.DecimalField(max_digits=15, decimal_places=13)
+    concreteness = models.DecimalField(max_digits=15, decimal_places=13)
+    aoa = models.DecimalField(max_digits=15, decimal_places=13)
+
+class ArticlesCan(models.Model):
+    #id is auto included
+    articleID = models.PositiveIntegerField()
+    language = models.CharField(max_length=255)
+    province = models.CharField(max_length=2)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=3)
+    publication_date = models.DateField()
+    word_count = models.PositiveIntegerField()
+    parsed_article = models.TextField()
+
+class DocumentData(models.Model):
+    articleID = models.PositiveIntegerField(primary_key = True)
+    language = models.CharField(max_length=255)
+    province = models.CharField(max_length=2)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=3)
+    publication_date = models.DateField()
+    word_count = models.PositiveIntegerField()
+    word_one = models.CharField(max_length=255, null=True, default="")
+    word_two = models.CharField(max_length=255, null=True, default="")
+    word_three = models.CharField(max_length=255, null=True, default="")
+    word_four = models.CharField(max_length=255, null=True, default="")
+    word_five = models.CharField(max_length=255, null=True, default="")
+    average_arousal_doc = models.DecimalField(max_digits=13, decimal_places=12)
+    average_valence_doc = models.DecimalField(max_digits=13, decimal_places=12)
+    average_arousal_words = models.DecimalField(max_digits=13, decimal_places=12, null=True, blank=True)
+    average_valence_words = models.DecimalField(max_digits=13, decimal_places=12, null=True, blank=True)
+  
+class WordData(models.Model):
+    word = models.CharField(max_length=255)
+    articleID = models.PositiveIntegerField()
+    word_count = models.PositiveIntegerField()
+    term_frequency = models.DecimalField(max_digits=14, decimal_places=12, null=True, blank=True)
+    tfidf = models.DecimalField(max_digits=14, decimal_places=12, null=True, blank=True)
+
+    class Meta:
+        unique_together = (("word", "articleID"),)
+
+class CorpusData(models.Model):
+    corpusID = models.PositiveIntegerField(primary_key = True)
+    start_date = models.DateField()
+    end_date = models.DateField()
---
+++ mysite/words/queries.py
@@ -0,0 +1,113 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views import generic
+from django.db import models
+from .models import SentimentDictionary, ArticlesCan, DocumentData, WordData, CorpusData
+
+def splitByLocationNoWord(givenProv = None, givenCity = None, givenCountry = None):
+    #results = DocumentData.objects
+    # I think it needs to be the articles we search due to DocumentData not having all the word data, so
+    results = ArticlesCan.objects
+    if(givenProv is not None):
+        results.filter(province=givenProv)
+    if(givenCity is not None):
+        results.filter(city=givenCity)
+    if(givenCountry is not None):
+        results.filter(country=givenCountry)
+    return results
+
+def splitByLocationOneWord(word, givenProv = None, givenCity = None, givenCountry = None):
+    results = ArticlesCan.objects
+    if(givenProv is not None):
+        results.filter(province=givenProv)
+    if(givenCity is not None):
+        results.filter(city=givenCity)
+    if(givenCountry is not None):
+        results.filter(country=givenCountry)
+    results.filter(parsed_article__contains=word)
+    return results
+
+def splitByLocationMultipleWords(words, givenProv = None, givenCity = None, givenCountry = None):
+    results = ArticlesCan.objects
+    if(givenProv is not None):
+        results.filter(province=givenProv)
+    if(givenCity is not None):
+        results.filter(city=givenCity)
+    if(givenCountry is not None):
+        results.filter(country=givenCountry)
+    for word in words:
+        results.filter(parsed_article__contains=word)
+    return results
+
+def splitByTimeNoWord(time1, time2):
+    return ArticlesCan.objects.filter(publication_date__range=[time1, time2])
+
+def splitByTimeNoWordTextOnly(time1, time2):
+    result = ArticlesCan.objects.filter(publication_date__range=[time1, time2]).values("parsed_article")
+    return result.split()
+
+def splitByTimeOneWord(word, time1, time2):
+    return ArticlesCan.objects.filter(publication_date__range=[time1, time2]).filter(parsed_article__contains=word)
+
+def splitByTimeOneWordTextOnly(word, time1, time2):
+    result = ArticlesCan.objects.filter(publication_date__range=[time1, time2]).filter(parsed_article__contains=word).values("parsed_article")
+    return result.split()
+
+def splitByTimeMultipleWords(words, time1, time2):
+    selection = ArticlesCan.objects.filter(publication_date__range=[time1, time2])
+    for word in words:
+        selection.filter(parsed_article__contains=word)
+    return selection
+
+def splitByTimeMultipleWordsTextOnly(words, time1, time2):
+    selection = ArticlesCan.objects.filter(publication_date__range=[time1, time2])
+    for word in words:
+        selection.filter(parsed_article__contains=word)
+    text = selection.values("parsed_article")
+    return text.split()
+
+# Easier just to get article IDs then get valence/arousal from that
+def getArticleIDsForGivenWord(word):
+    return ArticlesCan.objects.filter(parsed_article__contains=word).values("articleID")
+
+def getValence(docId):
+    return DocumentData.objects.get(articleID=docId).values("average_valence_doc")
+
+def getArousal(docId):
+    return DocumentData.objects.get(articleID=docId).values("average_arousal_doc")
+
+def getTop5Valence(docId):
+    return DocumentData.objects.get(articleID=docId).values("average_valence_words")
+
+def getTop5Arousal(docId):
+    return DocumentData.objects.get(articleID=docId).values("average_arousal_words")
+
+# Note that there can be multiple instances of the word in wordData
+def getWordData(givenWord):
+    return WordData.objects.filter(word = givenWord)
+
+# Still unsure of how this works.
+def getSimilarWords(word):
+    return None
+
+def getWordFrequency(word, time1=None, time2=None, givenProv = None, givenCity = None, givenCountry = None):
+    if(time1 is not None || time2 is not None):
+        result1 = self.splitByTimeOneWord(word, time1, time2)
+        result2 = self.splitByTimeNoWord(time1, time2)
+        return result1/result2
+    elif(givenProv is not None || givenCity is not None || givenCountry is not None):
+        result1 = self.splitByLocationOneWord(word, givenProv, givenCity, givenCountry)
+        result2 = self.splitByLocationNoWord(givenProv, givenCity, givenCountry)
+        return result1/result2
+    else:
+        return None
+
+# These 3 below should be implemented eventually.
+def getTFIDF(word):
+    return None
+
+def getRelativeWordFrequency(word):
+    return None
+
+def getPairwiseWords(word):
+    return None
