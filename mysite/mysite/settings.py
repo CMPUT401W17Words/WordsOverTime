66dab8fef5cebba8e8dd31e8883de94778d93f3d
tommy3ua <tommy3@ualberta.ca>
2017-03-15 11:30:45 -0600

    to server

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'Generated_Data',
-        #'USER': 'projectuser',
-        #'PASSWORD': '12345',
-        #'HOST': 'localhost',
-        #'PORT': '',
-    #}
     'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }    
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Generated_Data',
+        'USER': 'projectuser',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+    #'default': {
+            #'ENGINE': 'django.db.backends.sqlite3',
+            #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}    
 }
 
 
