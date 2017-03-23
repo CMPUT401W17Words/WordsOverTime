17fc70e6d690f39035f2ecab7165007bf3cf0952
tommy3ua <tommy3@ualberta.ca>
2017-03-23 09:47:58 -0600

    test

---
+++ mysite/words/productiontests.py
@@ -0,0 +1,16 @@
+from datetime import date
+import words.dataretrieval
+import words.requesthandler
+import words.databaseinput
+from words.models import Document_Data, Word_Data
+
+dateRange = (date(2000, 5, 19), date(2008, 10, 11))
+granularity = 'Year'
+word1 = 'credit'
+word2 = 'company'
+request = words.requesthandler.CosDistanceOverTimeRequest(dateRange, granularity, word1, word2, False)
+result = request.execute()
+print('cos distance over time')
+print(result.xValues)
+print(result.yValues)
+result.generateCSV(self.csvFilePath)
