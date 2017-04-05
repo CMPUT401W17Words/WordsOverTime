089f95e91ee602aeaaf8951b3eba8ac4a66756bd
tommy3ua <tommy3@ualberta.ca>
2017-04-05 11:14:13 -0600

    documentation

--- makeDocs.bat
+++ makeDocs.bat
@@ -2,3 +2,11 @@ pydoc -w words
 pydoc -w words.dataretrieval
 pydoc -w words.dataanalyzer
 pydoc -w words.requesthandler
+pydoc -w words.databaseinput
+pydoc -w words.emailsending
+pydoc -w words.forms
+pydoc -w words.hash
+pydoc -w words.models
+pydoc -w words.urls
+pydoc -w words.views
+
