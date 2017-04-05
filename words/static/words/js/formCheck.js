1ac57fb1b883c3214301811dd0fe54836e2133bc
GuoXingqi <xingqi1@cs.ualberta.ca>
2017-04-05 14:40:41 -0600

    timepicker and input validation

---
+++ words/static/words/js/formCheck.js
@@ -0,0 +1,18 @@
+function validatee_required(field,alerttxt)
+{
+with (field)
+  {
+  if (value==null||value=="")
+    {alert(alerttxt);return false}
+  else {return true}
+  }
+}
+
+function validate_form(thisform)
+{
+with (thisform)
+  {
+  if (validate_required(userEmail,"Email must be filled out!")==false)
+    {userEmail.focus();return false}
+  }
+}
